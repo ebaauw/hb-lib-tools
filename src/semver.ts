808037ce1509cd37d438f186e25114d276081b1d
ebaauw <erik.baauw@xs4all.nl>
2026-09-02 14:13:57 +0200

    Update semver.ts
    Convert to TypeScript

--- src/semver.ts
+++ src/semver.ts
@@ -1,13 +1,13 @@
-// hb-lib-tools/lib/chalk.js
+// hb-lib-tools/src/semver.ts
 //
 // Library for Homebridge plugins.
 // Copyright © 2016-2026 Erik Baauw. All rights reserved.
 
-import s from 'semver'
+import semver from 'semver'
 
 /** Return the [`semver`](https://github.com/npm/node-semver) module,
   * so plugins don't have to list this as a separate dependency.
   * @name semver
   * @memberof module:hb-lib-tools
   */
-export const semver = s
+export { semver }
