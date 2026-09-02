11fd35b8237b71727b07b120c49defbf15030b7e
ebaauw <erik.baauw@xs4all.nl>
2026-09-03 00:02:52 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -8,20 +8,21 @@ export default tseslint.config(
   {
     rules: {
       quotes: ['error', 'single'],
-      indent: ['error', 2, { SwitchCase: 0 }],
+      indent: ['error', 2, { SwitchCase: 1 }],
       'linebreak-style': ['error', 'unix'],
       semi: ['error', 'never'],
       'comma-dangle': ['error', 'never'],
       'dot-notation': 'error',
       eqeqeq: ['error', 'smart'],
       curly: ['error', 'all'],
-      'brace-style': ['error'],
+      'brace-style': ['error', '1tbs', { allowSingleLine: true }],
       'prefer-arrow-callback': 'warn',
       'max-len': ['warn', 160],
       'object-curly-spacing': ['error', 'always'],
-      'no-use-before-define': 'on',
+      'no-use-before-define': 'warn',
+      'no-empty': ['error', { allowEmptyCatch: true }],
       '@typescript-eslint/no-use-before-define': ['error', { classes: true, enums: true }],
-      '@typescript-eslint/no-unused-vars': ['error', { caughtErrors: 'all' }]
+      '@typescript-eslint/no-unused-vars': ['error', { caughtErrors: 'none' }]
     }
   },
   {
