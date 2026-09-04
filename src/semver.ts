30b03cb618fbeb5abe4b1b21a1a9628fdec1e804
ebaauw <erik.baauw@xs4all.nl>
2026-09-04 15:31:01 +0200

    Update semver.ts

--- src/semver.ts
+++ src/semver.ts
@@ -3,11 +3,16 @@
 // Library for Homebridge plugins.
 // Copyright © 2016-2026 Erik Baauw. All rights reserved.
 
+/** Return the [`semver`](https://github.com/npm/node-semver) module,
+  * so plugins don't have to install this as a separate dependency.
+  *
+  * To use `semver`, issue:
+  * ```typescript
+  * import { semver } from 'hb-lib-tools/semver'
+  * ```
+  * @module
+  */
+
 import semver from 'semver'
 
-/** Return the [`semver`](https://github.com/npm/node-semver) module,
-  * so plugins don't have to list this as a separate dependency.
-  * @name semver
-  * @memberof module:hb-lib-tools
-  */
 export { semver }
