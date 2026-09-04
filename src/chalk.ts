7f6e4182bc147d8fba94a79c3921fea5684321aa
ebaauw <erik.baauw@xs4all.nl>
2026-09-04 15:30:23 +0200

    Update chalk.ts

--- src/chalk.ts
+++ src/chalk.ts
@@ -3,14 +3,19 @@
 // Library for Homebridge plugins.
 // Copyright © 2016-2026 Erik Baauw. All rights reserved.
 
-import chalk from 'chalk'
+/** Return the [`chalk`](https://github.com/chalk/chalk) module,
+  * so plugins don't have to install this as a separate dependency.
+  * 
+  * To use `chalk`, issue:
+  * ```typescript
+  * import { chalk } from 'hb-lib-tools/chalk'
+  * ```
+  * @module
+  */
+
+import chalk, { ChalkInstance } from 'chalk'
 
 chalk.level = 2 // Force chalk to use 256 colours, even when not running in a terminal.
 
-/** Return the [`chalk`](https://github.com/chalk/chalk) module,
-  * so plugins don't have to list this as a separate dependency.
-  * @name chalk
-  * @memberof module:hb-lib-tools
-  */
-
+export type { ChalkInstance }
 export { chalk }
