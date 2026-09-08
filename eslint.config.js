a6bc56d44fe7a2c684c3ef1ae0081e3c7de62e13
ebaauw <erik.baauw@xs4all.nl>
2026-09-08 23:12:04 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,14 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import love from 'eslint-config-love'
 
-export default tseslint.config(
+export default [
   {
-    ignores: ['dist/**', 'doc/**', 'node_modules/**']
+    ...love,
+    files: ['**/*.ts', '**/*.js'],
+  },
+  {
+    ignores: ['dist/**', 'doc/**', 'test/**', 'node_modules/**']
   },
   {
     rules: {
@@ -12,6 +17,7 @@ export default tseslint.config(
       'linebreak-style': ['error', 'unix'],
       semi: ['error', 'never'],
       'comma-dangle': ['error', 'never'],
+      complexity: ['error', { max: 20 }],
       'dot-notation': 'error',
       eqeqeq: ['error', 'smart'],
       curly: ['error', 'all'],
@@ -21,16 +27,18 @@ export default tseslint.config(
       'object-curly-spacing': ['error', 'always'],
       'no-use-before-define': 'warn',
       'no-empty': ['error', { allowEmptyCatch: true }],
-      '@typescript-eslint/no-use-before-define': ['error', { classes: true, enums: true }],
-      '@typescript-eslint/no-unused-vars': ['error', { caughtErrors: 'none' }]
+      '@typescript-eslint/method-signature-style': ['error', 'method'],
+      '@typescript-eslint/no-magic-numbers': ['error', { ignore: [-1, 0, 1], ignoreArrayIndexes: true, ignoreDefaultValues: true }],
+      '@typescript-eslint/no-unused-vars': ['error', { caughtErrors: 'none' }],
+      '@typescript-eslint/no-use-before-define': ['error', { classes: true, enums: true }]
     }
   },
   {
     languageOptions: {
-      ecmaVersion: 2022,
+      ecmaVersion: 2024,
       sourceType: 'module'
     }
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended
-)
+]
